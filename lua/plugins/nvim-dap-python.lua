25f907f614973802986e6fff41c0d4c7f746b7af
Tsiry MAYET <tsiry.mayet@insa-rouen.fr>
2026-01-13 23:15:17 +0100

    update python debugger to not just my code

--- lua/plugins/nvim-dap-python.lua
+++ lua/plugins/nvim-dap-python.lua
@@ -1,6 +1,19 @@
 return {
   {
     "mfussenegger/nvim-dap-python",
-    config = function(_, opts) require("dap-python").setup "python" end,
+    config = function()
+      local dap_python = require "dap-python"
+      dap_python.setup "python"
+
+      -- Insert a new configuration with justMyCode = false
+      table.insert(require("dap").configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "Launch file (include libs)",
+        program = "${file}",
+        justMyCode = false, -- <—— here
+        console = "integratedTerminal",
+      })
+    end,
   },
 }
