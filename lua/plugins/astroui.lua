9b7a433f2b49b948735d09823152a95de5e8ecd1
Tsiry Mayet <mayet.tsiry@gmail.com>
2026-06-18 19:55:17 +0200

    update

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -34,7 +34,6 @@ return {
         SnacksPickerListBorder = { bg = "NONE" },
         SnacksPickerPreviewNormal = { bg = "NONE" },
         SnacksPickerPreviewBorder = { bg = "NONE" },
-        LineNr = { fg = "#000000" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
